8b4721bdcd71f25cadd91a012a6367b020a0ab91
Mai Vang <vmaineng@gmail.com>
2026-03-08 16:22:24 -0700

    added in supabase

---
+++ app/lib/supabase.ts
@@ -0,0 +1,29 @@
+import { createBrowserClient} from '@supabase/ssr'
+import {createServerClient, type CookieOptions} from '@supabase/ssr'
+import {cookies} from 'next/headers'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+export function createClient() {
+    return createBrowserClient(supabaseUrl, supabaseAnon)
+}
+
+export async function createServerSupabaseClient() {
+    const cookieStore = await cookies()
+
+    return createServerClient(supabaseUrl, supabaseAnon, {
+   cookies: {
+    getAll() {
+      return cookieStore.getAll()
+    },
+    setAll(cookiesToSet) {
+      try {
+        cookiesToSet.forEach(({ name, value, options }) =>
+          cookieStore.set(name, value, options)
+        )
+      } catch {}
+    },
+  },
+})
+}
