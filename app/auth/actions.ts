c859a7b858f1aedf2f324e54d9d909e4e96be32e
Mai Vang <vmaineng@gmail.com>
2026-03-08 18:22:56 -0700

    added in actions for signup

---
+++ app/auth/actions.ts
@@ -0,0 +1,56 @@
+'use server'
+
+import { revalidatePath } from "next/cache";
+import { createServerSupabaseClient } from "../../lib/supabase";
+import { redirect } from "next/navigation";
+
+export async function signUp(FormData: FormData) { 
+    const supabase = await createServerSupabaseClient();
+    const email = FormData.get("email") as string;
+    const password = FormData.get("password") as string;
+    const username= FormData.get("username") as string;
+
+    const { error} = await supabase.auth.signUp({
+        email,
+        password,
+        options: {
+            data: {
+                username,
+            }
+        }
+    })
+
+    if (error) {
+        console.error("Error signing up:", error);
+        throw new Error("Failed to sign up");
+    }
+
+    const {data: {user} } = await supabase.auth.getUser();
+    if (user) {
+        await supabase .from("profiles").update({username}).eq('id', user.id)
+    }
+    revalidatePath('/', 'layout')
+    redirect('/dashboard')
+}
+
+export async function signIn(FormData: FormData) {
+    const supabase = await createServerSupabaseClient();
+    const email = FormData.get("email") as string;
+    const password = FormData.get("password") as string;
+
+    const { error} = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    })
+    if (error) return console.error("Error signing in:", error);
+    revalidatePath('/', 'layout')
+    redirect('/dashboard')
+}
+
+export async function signOut() {
+    const supabase = await createServerSupabaseClient();
+    const { error} = await supabase.auth.signOut();
+    if (error) return console.error("Error signing out:", error);
+    revalidatePath('/', 'layout')
+    redirect('/')
+}
---
+++ app/auth/page.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+import { useState, useTransition } from "react";
+import Link from "next/link";
+import { Button, Input, Card } from "../ui";
